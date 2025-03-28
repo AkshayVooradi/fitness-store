9ea728767616d93781b5382309c873724e33331f
AkshayVooradi <av185236@ncr.com>
2025-03-28 22:20:46 +0530

    CRUD operations on orders

--- backend/src/main/java/com/fitnessStore/backend/StorageClasses/AddressClass.java
+++ backend/src/main/java/com/fitnessStore/backend/StorageClasses/AddressClass.java
@@ -9,8 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
-public class AddressClass {
+public class AddressEntity {
     private String streetAddress;
 
     private String city;
