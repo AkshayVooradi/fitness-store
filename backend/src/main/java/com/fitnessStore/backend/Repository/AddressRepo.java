553fad9628790f9868049f38d0f1d8185953a92d
AkshayVooradi <av185236@ncr.com>
2025-03-30 17:27:10 +0530

    search and filter functionality

--- backend/src/main/java/com/fitnessStore/backend/Repository/AddressRepo.java
+++
@@ -1,8 +0,0 @@
-package com.fitnessStore.backend.Repository;
-
-import com.fitnessStore.backend.StorageClasses.AddressClass;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AddressRepo extends MongoRepository<AddressClass,ObjectId> {
-}
